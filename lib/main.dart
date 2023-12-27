993d29f8281adc732cb7a8f28010cb3671e17907
godgiftedboy <61019893+godgiftedboy@users.noreply.github.com>
2023-12-27 22:00:13 +0545

    Basic Calculation app using Stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: non_constant_identifier_names
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,38 +19,99 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => MyHomeState();
+}
+
+class MyHomeState extends State<MyHomePage> {
+  var no1Controller = TextEditingController();
+  var no2Controller = TextEditingController();
+  var result = "";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.deepPurple,
-        title: const Center(
-          child: Text(
-            "Positioned widget",
-            style: TextStyle(color: Colors.white),
-            textAlign: TextAlign.center,
-          ),
-        ),
+        title: const Text("Basic Calculation app using Stateful widget"),
       ),
       body: Container(
-        width: 300,
-        height: 300,
-        color: Colors.blueGrey,
-        child: Stack(
-          children: [
-            Positioned(
-              bottom: 1,
-              left: 1,
-              child: Container(
-                width: 100,
-                height: 100,
-                color: Colors.white,
-              ),
+        color: Colors.blue,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+            child: Column(
+              children: [
+                TextField(
+                  keyboardType: TextInputType.number,
+                  controller: no1Controller,
+                ),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  controller: no2Controller,
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          var no1 = int.parse(no1Controller.text.toString());
+                          var no2 = int.parse(no2Controller.text.toString());
+                          var sum = no1 + no2;
+                          result = "The sum of $no1 and $no2 is $sum";
+                          setState(() {});
+                        },
+                        child: Text("Add"),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          var no1 = int.parse(no1Controller.text.toString());
+                          var no2 = int.parse(no2Controller.text.toString());
+                          var diff = no1 - no2;
+                          result = "The difference of $no1 and $no2 is $diff";
+                          setState(() {});
+                        },
+                        child: Text("Sub"),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          var no1 = int.parse(no1Controller.text.toString());
+                          var no2 = int.parse(no2Controller.text.toString());
+                          var mul = no1 * no2;
+                          result = "The product of $no1 and $no2 is $mul";
+                          setState(() {});
+                        },
+                        child: Text("Mul"),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          var no1 = int.parse(no1Controller.text.toString());
+                          var no2 = int.parse(no2Controller.text.toString());
+                          var div = no1 / no2;
+                          result =
+                              "The division of $no1 and $no2 is ${div.toStringAsFixed(5)}";
+                          setState(() {});
+                        },
+                        child: Text("Div"),
+                      ),
+                    ],
+                  ),
+                ),
+                Text(
+                  "$result",
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
