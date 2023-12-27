90b212499cebecf9ee5fb02ad027dfa3400343e9
godgiftedboy <61019893+godgiftedboy@users.noreply.github.com>
2023-12-27 21:23:52 +0545

    parent container with bluegrey color

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/widgets/rounded_button.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,76 +27,30 @@ class MyHomePage extends StatelessWidget {
         backgroundColor: Colors.deepPurple,
         title: const Center(
           child: Text(
-            "My App Bar",
+            "Positioned widget",
             style: TextStyle(color: Colors.white),
             textAlign: TextAlign.center,
           ),
         ),
       ),
-      body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          const FaIcon(
-            FontAwesomeIcons.amazon,
-            color: Colors.blue,
-            size: 50,
-          ),
-          SizedBox(
-            width: 150,
-            child: RoundedButton(
-              btnName: "Login",
-              icon: const Icon(
-                Icons.login,
-                color: Colors.white,
-              ),
-              callBack: () {
-                // ignore: avoid_print
-                print("Logged in Succesfully");
-              },
-              textStyle: const TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.blueGrey,
+        child: Stack(
+          children: [
+            Container(
+              width: 200,
+              height: 200,
+              color: Colors.red,
             ),
-          ),
-
-          //play button
-          SizedBox(
-            width: 150,
-            child: RoundedButton(
-              btnColor: Colors.red,
-              btnName: "Play",
-              icon: const Icon(
-                Icons.play_arrow,
-                color: Colors.white,
-              ),
-              callBack: () {
-                // ignore: avoid_print
-                print("Played");
-              },
-              textStyle: const TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.white,
             ),
-          ),
-
-          //press button
-          SizedBox(
-            width: 150,
-            child: RoundedButton(
-              btnColor: Colors.grey,
-              btnName: "Press",
-              callBack: () {
-                // ignore: avoid_print
-                print("Pressed");
-              },
-              textStyle: const TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ]),
+          ],
+        ),
       ),
     );
   }
